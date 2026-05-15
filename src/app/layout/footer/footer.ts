2cf8a2d7d90b0f19229b696b80a10e25a297d3ae
Albert Babaiani <babaianialbert@gmail.com>
2026-05-15 23:41:57 +0400

    fix footer focuses

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { InviteLinkComponent } from '../../shared/ui/invite-link/invite-link';
 
 @Component({
   selector: 'footer[app-footer]',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, InviteLinkComponent],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
